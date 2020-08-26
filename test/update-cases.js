c47bb66d3d4020ea5749f28042924d411158aaab
Andrey Sitnik <andrey@sitnik.ru>
2020-08-25 19:14:00 -0500

    Fix update cases script

--- test/update-cases.js
+++ test/update-cases.js
@@ -6,11 +6,9 @@ let { jsonify } = require('postcss-parser-tests')
 
 let parse = require('../parse')
 
-let dir = join(__dirname, 'test', 'cases')
+let dir = join(__dirname, 'cases')
 readdirSync(dir)
-  .filter(i => {
-    return extname(i) === '.sss'
-  })
+  .filter(i => extname(i) === '.sss')
   .forEach(name => {
     let sssFile = join(dir, name)
     let cssFile = sssFile.replace(/\.sss/, '.css')
