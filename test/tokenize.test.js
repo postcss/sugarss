48f7b152134aabd5f18e557ceef38980ec99c344
rittiev <rittiev@devexperts.com>
2024-11-19 10:15:38 +0000

    Refactored tests for tokenize.js

--- test/tokenize.test.js
+++ test/tokenize.test.js
@@ -4,71 +4,72 @@ let { Input } = require('postcss')
 
 let tokenize = require('../tokenize')
 
-function run(css, tokens) {
-  deepStrictEqual(tokenize(new Input(css)), tokens)
+function run(css, expectedTokens) {
+  let tokens = tokenize(new Input(css))
+  deepStrictEqual(tokens, expectedTokens)
 }
 
-test('tokenizes inine comments', () => {
-  run('// a', [['comment', '// a', 1, 1, 1, 4, 'inline']])
+test('tokenizes inline comments', () => {
+  run('// a', [['comment', '// a', 0, 4, 'inline']])
 })
 
-test('tokenizes inine comments and new lines', () => {
+test('tokenizes inline comments and new lines', () => {
   run('// a\r\n', [
-    ['comment', '// a', 1, 1, 1, 4, 'inline'],
-    ['newline', '\r\n', 1]
+    ['comment', '// a', 0, 4, 'inline'],
+    ['newline', '\r\n', 4, 6]
   ])
 })
 
-test('tokenizes new lines arround spaces', () => {
+test('tokenizes new lines around spaces', () => {
   run(' \n ', [
-    ['space', ' '],
-    ['newline', '\n', 1],
-    ['space', ' ']
+    ['space', ' ', 0, 1],
+    ['newline', '\n', 1, 2],
+    ['space', ' ', 2, 3]
   ])
 })
 
 test('tokenizes Windows new lines', () => {
-  run('\r\n', [['newline', '\r\n', 1]])
+  run('\r\n', [['newline', '\r\n', 0, 2]])
 })
 
 test('tokenizes single carriage return', () => {
   run('\ra', [
-    ['newline', '\r', 1],
-    ['word', 'a', 2, 1, 2, 1]
+    ['newline', '\r', 0, 1],
+    ['word', 'a', 1, 2]
   ])
 })
 
 test('tokenizes last carriage return', () => {
-  run('\r', [['newline', '\r', 1]])
+  run('\r', [['newline', '\r', 0, 1]])
 })
 
 test('tokenizes comma', () => {
   run('a,b', [
-    ['word', 'a', 1, 1, 1, 1],
+    ['word', 'a', 0, 1],
     [',', ',', 1, 2],
-    ['word', 'b', 1, 3, 1, 3]
+    ['word', 'b', 2, 3]
   ])
 })
 
 test('escapes control symbols', () => {
   run('\\(\\{\\"\\@\\\\""', [
-    ['word', '\\(', 1, 1, 1, 2],
-    ['word', '\\{', 1, 3, 1, 4],
-    ['word', '\\"', 1, 5, 1, 6],
-    ['word', '\\@', 1, 7, 1, 8],
-    ['word', '\\\\', 1, 9, 1, 10],
-    ['string', '""', 1, 11, 1, 12]
+    ['word', '\\(', 0, 2],
+    ['word', '\\{', 2, 4],
+    ['word', '\\"', 4, 6],
+    ['word', '\\@', 6, 8],
+    ['word', '\\\\', 8, 10],
+    ['string', '""', 10, 12]
   ])
 })
 
 test('escapes new line', () => {
   run('\\\n\\\r\\\r\n\\\f\\\\\n', [
-    ['word', '\\\n', 1, 1, 1, 2],
-    ['word', '\\\r', 2, 1, 2, 2],
-    ['word', '\\\r\n', 3, 1, 3, 3],
-    ['word', '\\\f', 4, 1, 4, 2],
-    ['word', '\\\\', 5, 1, 5, 2],
-    ['newline', '\n', 5]
+    ['word', '\\\n', 0, 2],
+    ['word', '\\\r', 2, 4],
+    ['word', '\\\r\n', 4, 7],
+    ['word', '\\\f', 7, 9],
+    ['word', '\\\\', 9, 11],
+    ['newline', '\n', 11, 12]
   ])
 })
 
